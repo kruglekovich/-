4286e17cb30c86b7dd808ec9aba5b845a21b61b1
Nikolay <nikolayharlov289@gmail.com>
2022-04-06 19:41:14 +0700

    sudent_group

---
+++ discipline.php
@@ -0,0 +1,16 @@
+<?php
+
+class discipline {
+	private $id;
+	private $title;
+
+	public function __construct($i, $t) {
+		$this->id = $i;
+		$this->title = $t;
+}
+
+    public function display(){
+		echo sprintf('%s<br>', $this->title);
+	}
+}
+?>
