d153666455e1f30a26bcbd417b257053a822fb50
Nikolay harlov <nikolayharlov289@gmail.com>
2022-04-08 13:03:05 +0700

    дисциплины

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 require_once 'student.php';
 require_once 'group.php';
 require_once 'discipline.php';
+require_once 'teacher.php';
 
 function loadStudents($group, $path){
 	$file = nl2br(file_get_contents($path));
@@ -31,6 +32,12 @@ $g3->addDiscipline($d3);
 $g3->addDiscipline($d4);
 
 loadStudents($g3, 'i03.txt');
-$g3->display();
+//$g3->display();
 
+$t1 = new Teacher(1, 'Власов',' Сергей',' Иванович' );
+$t2 = new Teacher(2, 'Черепанова',' Юлия',' Сергеевна');
+
+$t1->addDiscipline($d1);
+$t2->addDiscipline($d4);
+$t2->display();
 ?>
